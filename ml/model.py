6147ad503c1a913f490fb927036395ff23650ff9
yeha98555 <yeha98555@gmail.com>
2024-06-14 02:22:17 +0800

    feat: Move train_model and test_model to parent Model

--- ml/model.py
+++ ml/model.py
@@ -6,30 +6,6 @@ import torch.nn as nn
 
 
 class Model:
-    def train(self, X_train, y_train, num_epochs) -> np.array:
-        raise NotImplementedError
-
-    def test(
-        self, X_train, y_train, X_test, y_test, scaler
-    ) -> Tuple[np.array, np.array]:
-        raise NotImplementedError
-
-
-class LSTMModel(nn.Module, Model):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
-        super(LSTMModel, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
-        out = self.fc(out[:, -1, :])
-        return out
-
     def train_model(
         self, X_train, y_train, num_epochs, learning_rate=0.01, weight_decay=0.0001
     ) -> np.array:
@@ -95,3 +71,19 @@ class LSTMModel(nn.Module, Model):
         print(f"Train RMSE: {train_rmse}")
         print(f"Test RMSE: {test_rmse}")
         return train_predict, test_predict
+
+
+class LSTMModel(nn.Module, Model):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTMModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+        return out
